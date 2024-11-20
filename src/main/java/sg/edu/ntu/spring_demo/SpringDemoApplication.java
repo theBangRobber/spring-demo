5b0bd4d35004030b63ce2a3390f5faa431475688
Andrew To <to.andrew1@gmail.com>
2024-11-21 01:28:32 +0800

    Minor update to logger

--- src/main/java/sg/edu/ntu/spring_demo/SpringDemoApplication.java
+++ src/main/java/sg/edu/ntu/spring_demo/SpringDemoApplication.java
@@ -1,7 +1,7 @@
 package sg.edu.ntu.spring_demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
